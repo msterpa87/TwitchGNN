f7dc55bf902334c246a3894405ef3a8867920983
msterpa87 <sterpa.1152419@studenti.uniroma1.it>
2022-02-05 08:00:21 +0100

    fixed implementation using Cora dataset

---
+++ models.py
@@ -0,0 +1,40 @@
+from spektral.layers import GCNConv
+from tensorflow.keras.layers import Dropout, Dense, Input
+from tensorflow.keras import Model
+
+class GNNNodeClassifier(Model):
+    def __init__(self, n_labels=2, hidden_channels=64, dropout_rate=0.2):
+        super(GNNNodeClassifier, self).__init__()
+        self.conv1 = GCNConv(hidden_channels, activation='relu')
+        self.conv2 = GCNConv(hidden_channels, activation='relu')
+        self.conv3 = GCNConv(hidden_channels, activation='relu')
+        self.fn1 = Dense(hidden_channels, activation='relu')
+        self.fn2 = Dense(n_labels, activation='softmax')
+        self.dropout_rate = dropout_rate
+    
+    def call(self, inputs):
+        x, adj = inputs
+        
+        x = self.conv1([x, adj])
+        x = self.conv2([x, adj])
+        x = self.conv3([x, adj])
+        x = Dropout(self.dropout_rate)(x)
+        x = self.fn1(x)
+        x = Dropout(self.dropout_rate)(x)
+        x = self.fn2(x)
+
+        return x
+
+class BasicGCN(Model):
+    def __init__(self, n_labels=2, hidden_channels=64):
+        super(BasicGCN, self).__init__()
+        self.conv1 = GCNConv(hidden_channels, activation='relu')
+        self.conv2 = GCNConv(n_labels, activation='softmax')
+    
+    def call(self, inputs):
+        x, adj = inputs
+        
+        x = self.conv1([x, adj])
+        x = self.conv2([x, adj])
+        
+        return x
